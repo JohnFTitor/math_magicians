04ed26a76c3a4c8899d23efd87963ed9828f1603
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-09 12:46:31 -0500

    Add event handler to calculator buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import '../styles/calculator.css';
 import CalculatorButton from './CalculatorButton';
+import calculate from '../logic/calculate';
 // As required for this project, components will be defined class based
 
-// eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      calculatorObj: {},
+    };
+    this.calculateHandler = this.calculateHandler.bind(this);
+  }
+
+  calculateHandler(buttonName) {
+    this.setState((previousState) => ({
+      calculatorObj: calculate(previousState.calculatorObj, buttonName),
+    }));
+    const calculatorObj = this.state;
+    console.log(calculatorObj);
+  }
+
   render() {
     return (
       <div className="calculator-container">
         <input placeholder="0" disabled />
         <div className="calculator-buttons">
-          <CalculatorButton value="AC" />
-          <CalculatorButton value="+/-" />
-          <CalculatorButton value="%" />
-          <CalculatorButton value={'\u00F7'} isSign />
-          <CalculatorButton value="7" />
-          <CalculatorButton value="8" />
-          <CalculatorButton value="9" />
-          <CalculatorButton value="x" isSign />
-          <CalculatorButton value="4" />
-          <CalculatorButton value="5" />
-          <CalculatorButton value="6" />
-          <CalculatorButton value="-" isSign />
-          <CalculatorButton value="1" />
-          <CalculatorButton value="2" />
-          <CalculatorButton value="3" />
-          <CalculatorButton value="+" isSign />
-          <CalculatorButton value="0" />
-          <CalculatorButton value="." />
-          <CalculatorButton value="=" isSign />
+          <CalculatorButton value="AC" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="+/-" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="%" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="÷" calculateHandler={this.calculateHandler} isSign />
+          <CalculatorButton value="7" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="8" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="9" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="x" calculateHandler={this.calculateHandler} isSign />
+          <CalculatorButton value="4" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="5" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="6" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="-" calculateHandler={this.calculateHandler} isSign />
+          <CalculatorButton value="1" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="2" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="3" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="+" calculateHandler={this.calculateHandler} isSign />
+          <CalculatorButton value="0" calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="." calculateHandler={this.calculateHandler} />
+          <CalculatorButton value="=" calculateHandler={this.calculateHandler} isSign />
         </div>
       </div>
     );
--- src/components/CalculatorButton.js
+++ src/components/CalculatorButton.js
@@ -3,17 +3,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // As required for this project, components will be defined class based
-// eslint-disable-next-line react/prefer-stateless-function
+
 class CalculatorButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.calculateHandler = this.calculateHandler.bind(this);
+  }
+
+  calculateHandler() {
+    const { calculateHandler, value } = this.props;
+    calculateHandler(value);
+  }
+
   render() {
     const { value, isSign } = this.props;
     if (value === '0') {
-      return <button type="button" className="calculator-button zero-button">{value}</button>;
+      return <button type="button" className="calculator-button zero-button" onClick={this.calculateHandler}>{value}</button>;
     }
     if (isSign) {
-      return <button type="button" className="calculator-button sign-button">{value}</button>;
+      return <button type="button" className="calculator-button sign-button" onClick={this.calculateHandler}>{value}</button>;
     }
-    return <button type="button" className="calculator-button">{value}</button>;
+    return <button type="button" className="calculator-button" onClick={this.calculateHandler}>{value}</button>;
   }
 }
 
@@ -24,6 +34,7 @@ CalculatorButton.defaultProps = {
 CalculatorButton.propTypes = {
   value: PropTypes.string.isRequired,
   isSign: PropTypes.bool,
+  calculateHandler: PropTypes.func.isRequired,
 };
 
 export default CalculatorButton;
