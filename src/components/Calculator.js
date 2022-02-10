c781f6d6ff3b9c4f43c4993583602bcf2942ddbc
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-10 10:50:13 -0500

    Refactor calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,50 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+import React, { useState, useEffect } from 'react';
 import '../styles/calculator.css';
 import CalculatorButton from './CalculatorButton';
 import calculate from '../logic/calculate';
 import Display from './CalculatorDisplay';
 // As required for this project, components will be defined class based
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      calculatorObj: {},
-    };
-    this.calculateHandler = this.calculateHandler.bind(this);
+function Calculator() {
+  const [calculatorObj, setCalculatorObj] = useState({});
+
+  useEffect(() => setCalculatorObj({}));
+
+  function calculateHandler(buttonName) {
+    setCalculatorObj(() => calculate(calculatorObj, buttonName));
   }
 
-  componentWillUnmount() {
-    this.setState(() => ({
-      calculatorObj: {},
-    }));
-  }
-
-  calculateHandler(buttonName) {
-    this.setState((previousState) => ({
-      calculatorObj: calculate(previousState.calculatorObj, buttonName),
-    }));
-  }
-
-  render() {
-    const { calculatorObj } = this.state;
-    return (
-      <div className="calculator-container">
-        <Display
-          total={calculatorObj.total}
-          next={calculatorObj.next}
-          operation={calculatorObj.operation}
-        />
-        <div className="calculator-buttons">
-          <CalculatorButton value="AC" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="+/-" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="%" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="÷" calculateHandler={this.calculateHandler} isSign />
-          <CalculatorButton value="7" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="8" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="9" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="x" calculateHandler={this.calculateHandler} isSign />
-          <CalculatorButton value="4" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="5" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="6" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="-" calculateHandler={this.calculateHandler} isSign />
-          <CalculatorButton value="1" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="2" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="3" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="+" calculateHandler={this.calculateHandler} isSign />
-          <CalculatorButton value="0" calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="." calculateHandler={this.calculateHandler} />
-          <CalculatorButton value="=" calculateHandler={this.calculateHandler} isSign />
-        </div>
+  return (
+    <div className="calculator-container">
+      <Display
+        total={calculatorObj.total}
+        next={calculatorObj.next}
+        operation={calculatorObj.operation}
+      />
+      <div className="calculator-buttons">
+        <CalculatorButton value="AC" calculateHandler={calculateHandler} />
+        <CalculatorButton value="+/-" calculateHandler={calculateHandler} />
+        <CalculatorButton value="%" calculateHandler={calculateHandler} />
+        <CalculatorButton value="÷" calculateHandler={calculateHandler} isSign />
+        <CalculatorButton value="7" calculateHandler={calculateHandler} />
+        <CalculatorButton value="8" calculateHandler={calculateHandler} />
+        <CalculatorButton value="9" calculateHandler={calculateHandler} />
+        <CalculatorButton value="x" calculateHandler={calculateHandler} isSign />
+        <CalculatorButton value="4" calculateHandler={calculateHandler} />
+        <CalculatorButton value="5" calculateHandler={calculateHandler} />
+        <CalculatorButton value="6" calculateHandler={calculateHandler} />
+        <CalculatorButton value="-" calculateHandler={calculateHandler} isSign />
+        <CalculatorButton value="1" calculateHandler={calculateHandler} />
+        <CalculatorButton value="2" calculateHandler={calculateHandler} />
+        <CalculatorButton value="3" calculateHandler={calculateHandler} />
+        <CalculatorButton value="+" calculateHandler={calculateHandler} isSign />
+        <CalculatorButton value="0" calculateHandler={calculateHandler} />
+        <CalculatorButton value="." calculateHandler={calculateHandler} />
+        <CalculatorButton value="=" calculateHandler={calculateHandler} isSign />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
