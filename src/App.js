d92cb4228b0df2de3867a28fccd0af27f4237df2
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-15 17:37:42 -0500

    Update paths

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ const App = () => (
       <ul className="nav">
         <li id="logo"> Math Magicians </li>
         <ul className="nav nav-child">
-          <li><Link to="/"> Home </Link></li>
-          <li><Link to="/calculator"> Calculator </Link></li>
-          <li><Link to="/quote"> Quote </Link></li>
+          <li><Link to="/math_magicians"> Home </Link></li>
+          <li><Link to="/math_magicians/calculator"> Calculator </Link></li>
+          <li><Link to="/math_magicians/quote"> Quote </Link></li>
         </ul>
       </ul>
     </nav>
     <main>
       <Routes>
-        <Route path="calculator" element={<CalculatorPage />} />
-        <Route path="/" element={<Home />} />
-        <Route path="quote" element={<Quotes />} />
+        <Route path="math_magicians" element={<Home />} />
+        <Route path="math_magicians/calculator" element={<CalculatorPage />} />
+        <Route path="math_magicians/quote" element={<Quotes />} />
       </Routes>
     </main>
   </div>
